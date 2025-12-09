aeb44828366aae822e3cf22fe17c605e361a0e5b
Zach <zachary.dowling2027@gbwl.org>
2025-12-09 16:05:09 +0000

    3.3 Notes

---
+++ BuildABear.java
@@ -0,0 +1,21 @@
+/**
+ *  `BuildABear.java defines the BuildABear CLASS
+ * which is a template for BuildABear OBJECTS
+ */
+public class BuildABear {
+    // 3 SECTIONS IN AN OBJECT-CLASS!
+    // 1. INSTANCE VARIABLES (data/attributes/fields)
+    private String type;
+    private String name; 
+    private int fillLevel;
+    private double price;
+    private boolean hasSpeaker;
+    private String recording;
+    // ENCAPSULATION: keep variables PRIVATE (they can't be directly accessed outside of this CLASS)
+
+
+    // 2. CONSTRUCTORS (initialize values)
+
+    // 3. METHODS (behaviors/actions)
+
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+
+      //Create a new String -> SHORTCUT!
+      String lebron = "GOAT";
+      // But are actually OBJECTS
+      // The original way to create a String:
+      String steph = new String("GOAT");
+
+      // All objects can be created using this pattern:
+      // ClassName objectName = new ClassName();
+      BuildABear polar bear = new BuildABear("bear");
+
    }
 }
