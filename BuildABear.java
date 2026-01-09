5b66b18a63c12fc1df7e52485852670df9da4a13
Zach <zachary.dowling2027@gbwl.org>
2026-01-09 14:54:15 +0000

    3.6 Notes

--- BuildABear.java
+++ BuildABear.java
@@ -63,5 +63,17 @@ public class BuildABear {
         return state;
     }
 
+    // MUTATORS ("setters") are void, but accept an argument to change
+    // the value of an object's instance variable to something new
+
+    //only need setters for variables you WANT the user to change
+    public void setName(String newName) {
+        // assign the object's name to the new value
+        this.name = newName;
+    }
+    public void setFillLevel(int newFL) {
+        this.fillLevel = newFL;
+    }
+
 
 } // end class
